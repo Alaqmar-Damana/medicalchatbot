da79ff5e74d912be10f188ce6c43622c301b5acc
Alaqmar-Damana <alaqmardamana@gmail.com>
2024-02-29 14:03:02 +0530

    Store index added

---
+++ store_index.py
@@ -0,0 +1,26 @@
+import os
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+import pinecone
+from langchain.vectorstores import Pinecone
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+# print(PINECONE_API_ENV)
+# print(PINECONE_API_KEY)
+
+extracted_data = load_pdf("Data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+pinecone.init(api_key=PINECONE_API_KEY,
+              environment=PINECONE_API_ENV)
+
+index_name = "chatbotmedical"
+
+docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
